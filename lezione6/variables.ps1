8160619e933a720aee53e4914ed7518dffeba114
Davide-Lotito <80196658+Davide-Lotito@users.noreply.github.com>
2022-08-20 17:29:32 +0200

    firs commit
    Notes from the YT course

---
+++ lezione6/variables.ps1
@@ -0,0 +1,10 @@
+﻿$var1 = 5
+$var2 = 10
+$var3 = $var1 + $var2
+
+echo $var3
+
+## To stop the script
+Write-Host "Press any key to continue..."
+
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
